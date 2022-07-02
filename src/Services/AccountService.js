bd402463a74993570e819fa7dd8f3ff35a5d5f57
ThanhTiena <86332664+ThanhTiena@users.noreply.github.com>
2022-07-02 15:48:29 +0700

    update

--- src/Services/AccountService.js
+++ src/Services/AccountService.js
@@ -1,4 +1,9 @@
 export default function AccountService() {
 
     console.log("helllo");
+    console.log("aa")
+    let array = [0, 1, 2, 3, 4, 5];
+    array.forEach(element => {
+        console.log(element)
+    });
 }
